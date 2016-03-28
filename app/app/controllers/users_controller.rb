51b3677b18608d48fb20c090aa4a58a62508cb18
leandromac <contato@leandromacedo.com>
2016-03-28 15:46:05 -0300

    Create template form, edit and home + config route to root

--- app/app/controllers/users_controller.rb
+++ app/app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
@@ -15,6 +19,16 @@ class UsersController < ApplicationController
 				notice: 'Cadastro criado com sucesso!'
 		else
 			render action: :new
+		end
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to @user,
+				notice: 'Cadastro atualizado com sucesso!'
+		else
+			render action: :edit
 		end
 	end
 
